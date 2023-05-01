976f272742c5406411ca66731febbb8ce0b7a6bd
Rhythm Shandlya <armaanbgp@gmail.com>
2023-05-01 23:02:07 +0530

    f

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,11 +1,11 @@
-import httpStatus from 'http-status';
-import { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import catchAsync from '../utils/catchAsync';
-import ApiError from '../errors/ApiError';
-import pick from '../utils/pick';
-import { IOptions } from '../paginate/paginate';
-import * as userService from './user.service';
+import httpStatus from "http-status";
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import catchAsync from "../utils/catchAsync";
+import ApiError from "../errors/ApiError";
+import pick from "../utils/pick";
+import { IOptions } from "../paginate/paginate";
+import * as userService from "./user.service";
 
 export const createUser = catchAsync(async (req: Request, res: Response) => {
   const user = await userService.createUser(req.body);
@@ -13,32 +13,44 @@ export const createUser = catchAsync(async (req: Request, res: Response) => {
 });
 
 export const getUsers = catchAsync(async (req: Request, res: Response) => {
-  const filter = pick(req.query, ['name', 'role']);
-  const options: IOptions = pick(req.query, ['sortBy', 'limit', 'page', 'projectBy']);
+  const filter = pick(req.query, ["name", "role"]);
+  const options: IOptions = pick(req.query, [
+    "sortBy",
+    "limit",
+    "page",
+    "projectBy",
+  ]);
   const result = await userService.queryUsers(filter, options);
   res.send(result);
 });
 
 export const getUser = catchAsync(async (req: Request, res: Response) => {
-  if (typeof req.params['userId'] === 'string') {
-    const user = await userService.getUserById(new mongoose.Types.ObjectId(req.params['userId']));
+  if (typeof req.params["userId"] === "string") {
+    const user = await userService.getUserById(
+      new mongoose.Types.ObjectId(req.params["userId"])
+    );
     if (!user) {
-      throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+      throw new ApiError(httpStatus.NOT_FOUND, "User not found");
     }
     res.send(user);
   }
 });
 
 export const updateUser = catchAsync(async (req: Request, res: Response) => {
-  if (typeof req.params['userId'] === 'string') {
-    const user = await userService.updateUserById(new mongoose.Types.ObjectId(req.params['userId']), req.body);
+  if (typeof req.params["userId"] === "string") {
+    const user = await userService.updateUserById(
+      new mongoose.Types.ObjectId(req.params["userId"]),
+      req.body
+    );
     res.send(user);
   }
 });
 
 export const deleteUser = catchAsync(async (req: Request, res: Response) => {
-  if (typeof req.params['userId'] === 'string') {
-    await userService.deleteUserById(new mongoose.Types.ObjectId(req.params['userId']));
+  if (typeof req.params["userId"] === "string") {
+    await userService.deleteUserById(
+      new mongoose.Types.ObjectId(req.params["userId"])
+    );
     res.status(httpStatus.NO_CONTENT).send();
   }
 });
